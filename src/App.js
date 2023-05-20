00f50677bc4a7efc112cb9919228cfd60394a3cd
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-05-20 19:56:52 -0400

    dynamic form added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 
 import './App.css';
-import Login from './components/users/Login';
-import Register from './components/users/Register';
+import UserForm from './pages/UserForm';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi Wubit!</h1>
-      <Login />
-      <Register />
+      <UserForm />
     </div>
   );
 }
