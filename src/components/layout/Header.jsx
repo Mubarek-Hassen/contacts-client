6337ad968a14f3ac68da248a9c44dc774be5a6cb
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-05-22 21:22:33 -0400

    header added

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Favorite</li>
+          <li>Add new contact</li>
+        </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Header
