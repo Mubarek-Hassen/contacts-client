ebf1fecf9fccf16aee961de8f4286266b6538f9f
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-05-22 21:28:48 -0400

    layout added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 
 import './App.css';
+import Layout from './components/layout/Layout';
 import UserForm from './pages/UserForm';
 
 function App() {
   return (
     <div className="App">
+      <Layout>
       <UserForm />
+
+      </Layout>
+
     </div>
   );
 }
