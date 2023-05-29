2a1b5720d34940989312f1077d577732cf398401
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-05-28 21:29:44 -0400

    style setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import { Routes, Route } from "react-router-dom"
-import './App.css';
 import Layout from './components/layout/Layout';
 import UserForm from './pages/UserForm';
 import NewContact from "./components/contacts/NewContact";
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <Layout>
       <Routes>
         <Route path="/" element={<UserForm />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="/contacts/:id" element={<Contact />} />
+        {/* <Route path="/contacts" element={<Contacts />} />
+        <Route path="/contacts/:id" element={<Contact />} /> */}
         <Route path="/new-contact" element={<NewContact />} />
       </Routes>
       </Layout>
--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -3,12 +3,10 @@ import Classes from "./Header.module.css"
 import { Link } from 'react-router-dom'
 function Header() {
   return (
-      <nav>
-        <ul>
-        <li className={Classes.links}><Link to="/">Home</Link></li>
-        <li className={Classes.links}><Link to="/favorites">Favorites</Link></li>
-        <li className={Classes.links}><Link to="/new-contact">Add new contact</Link></li>
-        </ul>
+      <nav className={Classes.nav}>
+        <Link to="/">Home</Link>
+        <Link to="/favorites">Favorites</Link>
+        <Link to="/new-contact">Add new contact</Link>
       </nav>
   )
 }
