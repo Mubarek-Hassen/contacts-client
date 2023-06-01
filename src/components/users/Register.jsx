04821230778007501747b0d73fa9d9d4dfdbb49a
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-05-31 22:57:08 -0400

    input change handler added

--- src/components/users/Register.jsx
+++ src/components/users/Register.jsx
@@ -7,16 +7,21 @@ function Register() {
     password: "",
   })
   const { name, email, password } = formData;
+  const inputChangeHandler =(e)=>{
+    setFormData((prevState)=>({
+      ...prevState, [e.target.name]: e.target.value,
+    }))
+  }
   return (
     <div>
       <h2>Register</h2>
       <form>
         <label>Name </label>
-        <input name='name' value={name} type="text" placeholder='Name'/> <br />
+        <input name='name' value={name} type="text" placeholder='Name' onChange={inputChangeHandler} /> <br />
         <label>Email </label>
-        <input name='email' value={email} type="text" placeholder='Email'/> <br />
+        <input name='email' value={email} type="text" placeholder='Email' onChange={inputChangeHandler} /> <br />
         <label>Password </label>
-        <input name='password' value={password} type="password" placeholder='Password'/> <br />
+        <input name='password' value={password} type="password" placeholder='Password' onChange={inputChangeHandler} /> <br />
         <button type="submit">Register</button>
       </form>
     </div>
