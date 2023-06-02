a231fb866e66e8e2dfe32e811184bf7bdba305ab
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-06-01 21:11:54 -0400

    login change handler added

--- src/components/users/Login.jsx
+++ src/components/users/Login.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  })
+  const { email, password } = formData;
+  const inputChangeHandler =(e)=>{
+    setFormData((prevState)=>({
+      ...prevState, [e.target.name]: e.target.value,
+    }))
+  }
   return (
     <div>
       <h2>Login</h2>
       <form>
         <label>Email </label>
-        <input name='email' type="text" placeholder='Email' />
+        <input name='email' type="text" value={email} placeholder='Email' onChange={inputChangeHandler} />
         <br />
         <label>Password </label>
-        <input name='password' type="password" placeholder='Password' />
+        <input name='password' type="password" value={password} placeholder='Password' onChange={inputChangeHandler} />
         <br />
         <button type="submit">Login</button>
       </form>
