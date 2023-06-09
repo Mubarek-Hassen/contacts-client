4fca48b92e1d4ceb269d7862a9675fb89003d934
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-06-08 21:45:17 -0400

    routes fixed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom"
 import Layout from './components/layout/Layout';
-import UserForm from './pages/UserForm';
 import NewContact from "./components/contacts/NewContact";
+import Home from "./pages/Home";
+import Register from "./components/users/Register";
+import Login from "./components/users/Login";
 
 function App() {
   return (
     <div className="container">
       <Layout>
       <Routes>
-        <Route path="/" element={<UserForm />} />
+        <Route path="/" element={ <Home />}/>
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
         {/* <Route path="/contacts" element={<Contacts />} />
         <Route path="/contacts/:id" element={<Contact />} /> */}
         <Route path="/new-contact" element={<NewContact />} />
