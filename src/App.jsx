41ad5ca01db2a3d77562bff721e5548593ac894c
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-11-03 23:43:07 -0500

    router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home"
+import ContactList from "./pages/ContactList"
+import Contact from "./pages/Contact"
 
 function App() {
-
-
+  const router = createBrowserRouter([
+    {path: "/", element: <Home />},
+    {path: "/contact-list", element: <ContactList /> },
+    {path: "/contact", element: <Contact /> },
+  ])
   return (
     <>
-    <h1>Hi Here!</h1>
+    <RouterProvider router={ router } />
     </>
   )
 }
