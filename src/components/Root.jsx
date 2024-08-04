9be8210f9fc27fbcd9838283bf317a335573e1c8
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2024-08-04 14:13:53 -0400

    auto logout pt1

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,7 +1,21 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useLoaderData, useSubmit } from "react-router-dom"
 import Header from "./Header"
+import { useEffect } from "react"
 
 function Root() {
+  const token = useLoaderData()
+  const submit = useSubmit()
+  useEffect(()=>{
+    if(!token){
+      return;
+    }
+
+    setTimeout(() => {
+      submit(null, {action: "/logout",method: "post"})
+    }, 1*60*60*1000);
+
+  }, [token, submit])
+
   return (
     <>
     <Header />
