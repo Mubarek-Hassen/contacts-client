71b03d9477e9d7191c77f8c6e3b7410147de9c7d
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2024-08-16 13:40:30 -0400

    header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { NavLink, useRouteLoaderData, useSubmit } from "react-router-dom";
-import Button from "./Button";
+// import Button from "./Button";
 
 function Header() {
   const submit = useSubmit();
@@ -11,15 +11,15 @@ function Header() {
   const token = useRouteLoaderData("root");
 
   return (
-    <header className="flex justify-between items-center bg-indigo-300 w-full">
-      <h2 className="mx-8 text-4xl font-serif">I-Contact</h2>
+    <header className="flex justify-between items-center p-4 bg-indigo-300 w-full">
+      <h2 className="mx-8 text-4xl font-semibold font-serif">I-Contact</h2>
       <nav className="mx-8">
-        <ul className="flex gap-4 m-2 ">
+        <ul className="flex justify-between items-center gap-4 m-2 text-xl ">
           <li>
             <NavLink
               className={({ isActive }) =>
                 isActive
-                  ? "w-fit text-center my-8 border-black border-2 p-2 m-auto hover:text-slate-200 text-slate-900 hover:bg-slate-800 bg-slate-100 rounded"
+                  ? " border-black border-2 p-2 hover:text-slate-200 text-slate-900 hover:bg-slate-800 bg-slate-100 rounded"
                   : "w-fit text-center my-8 border-black border-2 p-2 m-auto text-slate-200 hover:text-slate-900 bg-slate-800 hover:bg-slate-100 rounded"
               }
               to="/"
@@ -33,7 +33,7 @@ function Header() {
               <NavLink
                 className={({ isActive }) =>
                   isActive
-                    ? "w-fit text-center my-8 border-black border-2 p-2 m-auto hover:text-slate-200 text-slate-900 hover:bg-slate-800 bg-slate-100 rounded"
+                    ? "w-fit text-center my-8 border-black border-2 p-2 m-auto  hover:text-slate-200 text-slate-900 hover:bg-slate-800 bg-slate-100 rounded"
                     : "w-fit text-center my-8 border-black border-2 p-2 m-auto text-slate-200 hover:text-slate-900 bg-slate-800 hover:bg-slate-100 rounded"
                 }
                 to="contacts"
@@ -44,14 +44,12 @@ function Header() {
           )}
           {!token && (
             <li>
-              <NavLink to="/auth?mode=login">Log In</NavLink>
+              <NavLink className={({isActive})=> isActive ? "border-black border-2 p-2 bg-slate-900 underline text-green-500 hover:bg-slate-200 hover:text-slate-900 rounded" :"border-black border-2 p-2 bg-black text-green-500 hover:bg-slate-200 hover:text-slate-900 rounded" }to="/auth?mode=login">Log In</NavLink>
             </li>
           )}
 
           {token && (
-            <li>
-              <button onClick={logOutHandler}>Logout</button>
-            </li>
+            <button onClick={logOutHandler} className="border-black border-2 p-2 bg-black text-green-500 hover:bg-slate-200 hover:text-slate-900 rounded">Logout</button>
           )}
         </ul>
       </nav>
--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -2,7 +2,7 @@
 
 const Wrapper = ({children}) => {
   return (
-    <div className="bg-amber-600 flex justify-center items-center flex-col  border-black border-2 my-12 w-3/4 m-auto p-4 min-h-96 ">
+    <div className="bg-amber-300 flex justify-center items-center flex-col  border-black border-2 my-12 w-3/4 m-auto p-4 min-h-96 ">
       {children}
     </div>
   )
