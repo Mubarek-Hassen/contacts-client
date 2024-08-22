3f614bc5f290087f596a57289d80abbf866ba2b9
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2024-08-21 22:25:21 -0400

    responsive continued

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,8 +4,8 @@ import Input from "./Input"
 const ContactForm = ({ method, contact, formType }) => {
   return (
 <Wrapper>
-  <h1 className="text-3xl">{formType}</h1>
-    <Form method={method} className="w-3/4 flex justify-center flex-col m-auto  my-4 ">
+  <h1 className=" text-2xl sm:text-3xl">{formType}</h1>
+    <Form method={method} className="w-3/4 flex justify-center flex-col m-auto text-left sm:text-center  my-4 ">
       <Input
         type="text"
         aria-label="name"
@@ -13,6 +13,7 @@ const ContactForm = ({ method, contact, formType }) => {
         label="Name"
         name="name"
         defaultValue={contact ? contact.name : ""}
+        required
       />
       <Input
         type="text"
@@ -21,10 +22,9 @@ const ContactForm = ({ method, contact, formType }) => {
         label="Profession"
         name="profession"
         defaultValue={contact ? contact.profession : ""}
+        required
       />
-      <br />
-      <br />
-      <button className="w-fit text-center my-8 border-black border-2 p-2 m-auto text-slate-200 hover:text-slate-900 bg-slate-800 hover:bg-slate-100 rounded" type="submit">Save Contact</button>
+      <button className="w-fit mt-4 text-center  border-black border-2 p-2 m-auto text-slate-200 hover:text-slate-900 bg-slate-800 hover:bg-slate-100 rounded" type="submit">Save Contact</button>
       <br />
       <br />
     </Form>
